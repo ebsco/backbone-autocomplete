0e8c30e3ac7c7b8d32c07e8ead73e9e3d0af5332
Ryan B Leach <rleach@ebsco.com>
2013-10-22 07:47:00 -0400

    implement umd and tag

--- src/backbone.autocomplete.js
+++ src/backbone.autocomplete.js
@@ -1,158 +1,180 @@
-var AutoCompleteItemView = Backbone.View.extend({
-    tagName: "li",
-    template: _.template('<a href="#"><%= label %></a>'),
+(function (root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(['backbone','underscore'], factory);
+	} else {
+		// Browser globals
+		var lib = factory(root.Backbone, root._);
+		root.AutoCompleteItemView = lib.AutoCompleteItemView
+		root.AutoCompleteView = lib.AutoCompleteView;
+	}
+}(this, function (Backbone, _) {
+	Backbone = Backbone || window.Backbone;
+	_ = _ || window._;
 
-    events: {
-        "click": "select"
-    },
+	var AutoCompleteItemView = Backbone.View.extend({
+		tagName: "li",
+		template: _.template('<a href="#"><%= label %></a>'),
 
-    render: function () {
-        this.$el.html(this.template({
-            "label": this.model.label()
-        }));
-        return this;
-    },
+		events: {
+			"click": "select"
+		},
 
-    select: function () {
-        this.options.parent.hide().select(this.model);
-        return false;
-    }
+		render: function () {
+			this.$el.html(this.template({
+				"label": this.model.label()
+			}));
+			return this;
+		},
 
-});
+		select: function () {
+			this.options.parent.hide().select(this.model);
+			return false;
+		}
 
-var AutoCompleteView = Backbone.View.extend({
-    tagName: "ul",
-    className: "autocomplete",
-    wait: 300,
+	});
 
-    queryParameter: "query",
-    minKeywordLength: 2,
-    currentText: "",
-    itemView: AutoCompleteItemView,
+	var AutoCompleteView = Backbone.View.extend({
+		tagName: "ul",
+		className: "autocomplete",
+		wait: 300,
 
-    initialize: function (options) {
-        _.extend(this, options);
-        this.filter = _.debounce(this.filter, this.wait);
-    },
+		queryParameter: "query",
+		minKeywordLength: 2,
+		currentText: "",
+		itemView: AutoCompleteItemView,
 
-    render: function () {
-        // disable the native auto complete functionality
-        this.input.attr("autocomplete", "off");
+		initialize: function (options) {
+			_.extend(this, options);
+			this.filter = _.debounce(this.filter, this.wait);
+		},
 
-        this.$el.width(this.input.outerWidth());
+		render: function () {
+			// disable the native auto complete functionality
+			this.input.attr("autocomplete", "off");
 
-        this.input
-            .keyup(_.bind(this.keyup, this))
-            .keydown(_.bind(this.keydown, this))
-            .after(this.$el);
+			this.$el.width(this.input.outerWidth());
 
-        return this;
-    },
+			this.input
+				.keyup(_.bind(this.keyup, this))
+				.keydown(_.bind(this.keydown, this))
+				.after(this.$el);
 
-    keydown: function (event) {
-        if (event.keyCode == 38) return this.move(-1);
-        if (event.keyCode == 40) return this.move(+1);
-        if (event.keyCode == 13) return this.onEnter();
-        if (event.keyCode == 27) return this.hide();
-    },
+			return this;
+		},
 
-    keyup: function () {
-        var keyword = this.input.val();
-        if (this.isChanged(keyword)) {
-            if (this.isValid(keyword)) {
-                this.filter(keyword);
-            } else {
-                this.hide()
-            }
-        }
-    },
+		keydown: function (event) {
+			if (event.keyCode == 38) return this.move(-1);
+			if (event.keyCode == 40) return this.move(+1);
+			if (event.keyCode == 13) return this.onEnter();
+			if (event.keyCode == 27) return this.hide();
+		},
 
-    filter: function (keyword) {
-    	var keyword = keyword.toLowerCase();
-        if (this.model.url) {
+		keyup: function () {
+			var keyword = this.input.val();
+			if (this.isChanged(keyword)) {
+				if (this.isValid(keyword)) {
+					this.filter(keyword);
+				} else {
+					this.hide()
+				}
+			}
+		},
 
-            var parameters = {};
-            parameters[this.queryParameter] = keyword;
+		filter: function (keyword) {
+			var keyword = keyword.toLowerCase();
+			if (this.model.url) {
 
-            this.model.fetch({
-                success: function () {
-                    this.loadResult(this.model.models, keyword);
-                }.bind(this),
-                data: parameters
-            });
+				var parameters = {};
+				parameters[this.queryParameter] = keyword;
 
-        } else {
-            this.loadResult(this.model.filter(function (model) {
-                return model.label().toLowerCase().indexOf(keyword) !== -1
-            }), keyword);
-        }
-    },
+				this.model.fetch({
+					success: function () {
+						this.loadResult(this.model.models, keyword);
+					}.bind(this),
+					data: parameters
+				});
 
-    isValid: function (keyword) {
-        return keyword.length > this.minKeywordLength
-    },
+			} else {
+				this.loadResult(this.model.filter(function (model) {
+					return model.label().toLowerCase().indexOf(keyword) !== -1
+				}), keyword);
+			}
+		},
 
-    isChanged: function (keyword) {
-        return this.currentText != keyword;
-    },
+		isValid: function (keyword) {
+			return keyword.length > this.minKeywordLength
+		},
 
-    move: function (position) {
-        var current = this.$el.children(".active"),
-            siblings = this.$el.children(),
-            index = current.index() + position;
-        if (siblings.eq(index).length) {
-            current.removeClass("active");
-            siblings.eq(index).addClass("active");
-        }
-        return false;
-    },
+		isChanged: function (keyword) {
+			return this.currentText != keyword;
+		},
 
-    onEnter: function () {
-        this.$el.children(".active").click();
-        return false;
-    },
+		move: function (position) {
+			var current = this.$el.children(".active"),
+				siblings = this.$el.children(),
+				index = current.index() + position;
+			if (siblings.eq(index).length) {
+				current.removeClass("active");
+				siblings.eq(index).addClass("active");
+			}
+			return false;
+		},
 
-    loadResult: function (model, keyword) {
-        this.currentText = keyword;
-        this.show().reset();
-        if (model.length) {
-            _.forEach(model, this.addItem, this);
-            this.show();
-        } else {
-            this.hide();
-        }
-    },
+		onEnter: function () {
+			this.$el.children(".active").click();
+			return false;
+		},
 
-    addItem: function (model) {
-        this.$el.append(new this.itemView({
-            model: model,
-            parent: this
-        }).render().$el);
-    },
+		loadResult: function (model, keyword) {
+			this.currentText = keyword;
+			this.show().reset();
+			if (model.length) {
+				_.forEach(model, this.addItem, this);
+				this.show();
+			} else {
+				this.hide();
+			}
+		},
 
-    select: function (model) {
-        var label = model.label();
-        this.input.val(label);
-        this.currentText = label;
-        this.onSelect(model);
-    },
+		addItem: function (model) {
+			this.$el.append(new this.itemView({
+				model: model,
+				parent: this
+			}).render().$el);
+		},
 
-    reset: function () {
-        this.$el.empty();
-        return this;
-    },
+		select: function (model) {
+			var label = model.label();
+			this.input.val(label);
+			this.currentText = label;
+			this.onSelect(model);
+		},
 
-    hide: function () {
-        this.$el.hide();
-        return this;
-    },
+		reset: function () {
+			this.$el.empty();
+			return this;
+		},
 
-    show: function () {
-        this.$el.show();
-        return this;
-    },
+		hide: function () {
+			this.$el.hide();
+			return this;
+		},
 
-    // callback definitions
-    onSelect: function () {}
+		show: function () {
+			this.$el.show();
+			return this;
+		},
 
-});
+		// callback definitions
+		onSelect: function () {}
+
+	});
+
+	return {
+		AutoCompleteView: AutoCompleteView,
+		AutoCompleteItemView: AutoCompleteItemView
+	}
+
+}));
+
